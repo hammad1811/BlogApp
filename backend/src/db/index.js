5953a7d77bf322e6051eaa8753aad7d65be479b4
hammad1811 <hammadsaleem1811@gmai..com>
2025-04-06 08:39:00 +0500

    fix: update main entry point in package.json and enhance MongoDB connection options

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-       const connection = await mongoose.connect(process.env.MONGODB_URI);
+       const connection = await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
         console.log('MongoDB connected successfully' + connection.connection.host);
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
